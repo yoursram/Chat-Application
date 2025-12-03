885242b13925f6fa4384a4dc1f4abc3023dba5cf
Ram Kumar <yoursram07@gmail.com>
2025-12-03 21:06:55 +0530

    Use environment variable for MongoDB connection URL

--- server/config/database.js
+++ server/config/database.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
-// const connectionURL = "mongodb+srv://ram123:ram@1234@chat-application.nwh8goy.mongodb.net/"
 const connectDB = async ()=>{
  try{
-    await mongoose.connect("mongodb+srv://ram123:ram%401234@chat-application.nwh8goy.mongodb.net/");
+    await mongoose.connect(process.env.connectionURL);
     console.log("DataBase connected ...");
  }catch(err){
     console.log("Database Error :",err.message);
